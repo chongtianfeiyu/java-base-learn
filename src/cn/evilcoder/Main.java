e1020a093f0b9b6bdb1e98e479a745b9c03c16fb
huangshanqi <hsqmobile@gmail.com>
2016-06-11 20:24:18 +0800

    add md5 utils

--- src/cn/evilcoder/Main.java
+++ src/cn/evilcoder/Main.java
@@ -1,5 +1,6 @@
 package cn.evilcoder;
 
+import cn.evilcoder.encryption.MD5;
 import cn.evilcoder.hash.Time33;
 
 public class Main {
@@ -10,6 +11,10 @@ public class Main {
         String str2 = "abd";
         System.out.println(str1 + "=" + Time33.getTime33(str1));
         System.out.println(str2 + "=" + Time33.getTime33(str2));
+
+        System.out.println(str1 + "=" + Time33.getTime33(MD5.md5(str1)));
+        System.out.println(str2 + "=" + Time33.getTime33(MD5.md5(str2)));
+
     }
 
 }
--- src/cn/evilcoder/encryption/MD5.java
+++ src/cn/evilcoder/encryption/MD5.java
@@ -1,9 +1,46 @@
 package cn.evilcoder.encryption;
 
+import java.security.MessageDigest;
+
 /**
  * User: evilcoder
  * Date: 2016/6/11
  * Time: 17:56
  */
 public class MD5 {
+  private final static String[] digits = {"0", "1", "2", "3", "4",
+      "5", "6", "7", "8", "9",
+      "a", "b", "c", "d", "e", "f"};
+
+  public static String byte2HexString(byte b) {
+    int n = b;
+    if (n < 0) {
+      n = 256 + n;
+    }
+    int d1 = n / 16;
+    int d2 = n % 16;
+    return digits[d1] + digits[d2];
+  }
+
+  public static String byteArrayToHexString(byte[] bytes) {
+    StringBuffer sb = new StringBuffer();
+    for (byte b : bytes) {
+      sb.append(byte2HexString(b));
+    }
+    return sb.toString();
+  }
+
+  public static String md5(String origin) {
+    String result = "";
+    try {
+      result = origin;
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      md.update(result.getBytes("UTF-8"));
+      result = byteArrayToHexString(md.digest());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return result;
+  }
+
 }
